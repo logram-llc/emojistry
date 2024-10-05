6c19bb9bdc65a1a6774fcbf872e2ec90114ce936
jramsey <jordan@logram.io>
2024-10-05 17:38:14 -0500

    Integrate Google Analytics 4 (#14)
    * chore: Add Google Analytics
    
    * chore(.gitignore): Add `.env`
    
    * chore: Don't use `declare global`
    
    * refactor(Analytics): Rename component to `GoogleAnalytics`

---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+
+type AnalyticsProps = {
+  gTagId: string;
+};
+
+declare const window: Window &
+  typeof globalThis & {
+    dataLayer: unknown[];
+  };
+
+export function GoogleAnalytics({ gTagId }: AnalyticsProps): null {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${gTagId}`;
+
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+
+      function gtag(...args: unknown[]) {
+        window.dataLayer.push(args);
+      }
+
+      gtag('js', new Date());
+      gtag('config', gTagId);
+    };
+
+    return () => {
+      const existingScript = document.querySelector(
+        `script[src^="https://www.googletagmanager.com/gtag/js"]`,
+      );
+
+      if (existingScript) {
+        existingScript.remove();
+      }
+    };
+  }, [gTagId]);
+
+  return null;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
+import { GoogleAnalytics } from './components/GoogleAnalytics';
 import { EmojiFamilyProvider } from '@/providers/EmojiFamilyProvider';
 import { EmojiGridSettingsProvider } from '@/providers/EmojiGridSettingsProvider';
 import { PictureInPictureProvider } from '@/providers/PictureInPictureProvider';
@@ -9,8 +10,11 @@ const container = document.getElementById('root');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
 
+const gTagId = import.meta.env.VITE_GA_TAG_ID;
+
 root.render(
   <React.StrictMode>
+    {gTagId && <GoogleAnalytics gTagId={gTagId}></GoogleAnalytics>}
     <PictureInPictureProvider>
       <EmojiFamilyProvider>
         <EmojiGridSettingsProvider>
