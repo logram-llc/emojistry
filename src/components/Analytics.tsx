ec6a645754bac2aadd1cd6501d8db6b131bfa5cd
Jordan Ramsey <jordan@logram.io>
2024-10-05 17:13:54 -0500

    chore: Add Google Analytics

---
+++ src/components/Analytics.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    dataLayer: unknown[];
+  }
+}
+
+type AnalyticsProps = {
+  gTagId: string;
+};
+
+export function Analytics({ gTagId }: AnalyticsProps): null {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${gTagId}`;
+
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+
+      function gtag(...args: unknown[]) {
+        window.dataLayer.push(args);
+      }
+
+      gtag('js', new Date());
+      gtag('config', gTagId);
+    };
+
+    return () => {
+      const existingScript = document.querySelector(
+        `script[src^="https://www.googletagmanager.com/gtag/js"]`,
+      );
+
+      if (existingScript) {
+        existingScript.remove();
+      }
+    };
+  }, [gTagId]);
+
+  return null;
+}
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GA_TAG_ID: string | undefined;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
