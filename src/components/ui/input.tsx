5dcfd2ff09d43075873a692c5e883b084584895b
Nicholas Ramsey <nicholas@logram.io>
2024-10-07 22:23:37 -0500

    feat(input): Icon support (#23)

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,20 +3,31 @@ import { forwardRef, InputHTMLAttributes } from 'react';
 import { cn } from '@/lib/utils';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  icon?: React.ReactElement;
+}
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ icon, className, type, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          'flex h-9 w-full rounded-lg border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50',
-          className,
+      <div className="w-full relative">
+        <input
+          type={type}
+          className={cn(
+            'flex h-9 w-full rounded-lg border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50',
+            className,
+            { 'pl-7': icon },
+          )}
+          ref={ref}
+          {...props}
+        />
+
+        {icon && (
+          <div className="absolute flex items-center justify-center top-0 bottom-0 left-2 text-muted-foreground">
+            {icon}
+          </div>
         )}
-        ref={ref}
-        {...props}
-      />
+      </div>
     );
   },
 );
