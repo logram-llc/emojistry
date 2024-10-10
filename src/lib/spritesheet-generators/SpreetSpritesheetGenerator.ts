e910b4e9608039841fded957c279f5538132a886
Nicholas Ramsey <nicholas@logram.io>
2024-10-09 23:48:52 -0500

    perf(SpreetSpritesheetGenerator): Convert png spritesheets to webp using cwebp

--- src/lib/spritesheet-generators/SpreetSpritesheetGenerator.ts
+++ src/lib/spritesheet-generators/SpreetSpritesheetGenerator.ts
@@ -1,4 +1,4 @@
-import { readFile } from 'fs/promises';
+import { readFile, rm } from 'fs/promises';
 import { exec } from 'child_process';
 import { promisify } from 'util';
 import {
@@ -14,17 +14,21 @@ export class SpreetSpritesheetGenerator implements ISpritesheetGenerator {
     outputPath,
     imagesInputPath,
   }: ISpritesheetGeneratorArgs): Promise<Record<string, ISpritesheetEmoji>> {
-    try {
-      const { stdout } = await execAsync(
-        `spreet --retina ${imagesInputPath} ${outputPath}`,
-      );
-      console.log(`spreet: ${stdout || 'Done'}`);
-    } catch (error) {
-      console.error(
-        `spreet: ${error.stderr?.toString() || error.message || 'Error'}`,
-      );
-      process.exit(1);
-    }
+    // NOTE(nicholas-ramsey): Spreet creates the PNG spritesheet based off of `outputPath`.
+    // It appends the `.png` file extension.
+    const spritesheetPngPath = `${outputPath}.png`;
+
+    const { stdout: spreetStdout } = await execAsync(
+      `spreet --retina ${imagesInputPath} ${outputPath}`,
+    );
+    console.log(`spreet: ${spreetStdout || 'Done'}`);
+
+    const { stdout: cwebpStdout } = await execAsync(
+      `cwebp -quiet -m 6 -mt -exact -near_lossless 60 -z 9 ${spritesheetPngPath} -o ${outputPath}.webp`,
+    );
+    console.log(`cwebp: ${cwebpStdout || 'Done'}`);
+
+    rm(spritesheetPngPath, { force: true });
 
     return JSON.parse(
       await readFile(`${outputPath}.json`, { encoding: 'utf8' }),
